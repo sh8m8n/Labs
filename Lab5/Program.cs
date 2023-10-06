a95c145169644b8f82120a7fa57ad308c132dfef
sh8m8n <118104171+sh8m8n@users.noreply.github.com>
2023-10-06 22:17:09 +0700

    Durak update 1 (GetRandomArray, WriteArray)

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -11,6 +11,7 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
+            Durak.WriteArray()
         }
     }
 }
--- LabLib/Durak.cs
+++ LabLib/Durak.cs
@@ -8,9 +8,53 @@ namespace LabLib
 {
     public static class Durak
     {
-        public static void F()
+        /// <summary>
+        /// Возвращает массив случайной длинны со случайными значениями в заданных пределах
+        /// </summary>
+        /// <param name="minElementCount"></param>
+        /// <param name="maxElementCount"></param>
+        /// <param name="minElementValue"></param>
+        /// <param name="maxElementValue"></param>
+        /// <returns></returns>
+        public static int[] GetRandomArray(int minElementCount, int maxElementCount, int minElementValue, int maxElementValue)
         {
-            Console.WriteLine("dslfkgjbljhdsfbg");
+            Random rnd = new Random();
+            int[] numbers = new int[rnd.Next(minElementCount, maxElementCount)];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(minElementValue, maxElementValue);
+            }
+            return numbers;
+        }
+
+        /// <summary>
+        /// Печатает массив в консоль
+        /// </summary>
+        /// <param name="array"></param>
+        public static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+        public static void WriteArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i != array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
