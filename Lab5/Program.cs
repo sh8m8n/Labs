21e4a5fdd51bb7ec2beebe4a4b8b0606ac11f70a
sh8m8n <118104171+sh8m8n@users.noreply.github.com>
2023-10-06 21:09:49 +0700

    add LabLibrary

---
+++ Lab5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabLib;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LabLib/Durak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabLib
+{
+    public static class Durak
+    {
+        public static void F()
+        {
+            Console.WriteLine("dslfkgjbljhdsfbg");
+        }
+    }
+}
