eda4e714e7b4290040241fb93877aec17a0c32d7
Octomor <stepanka4444@gmail.com>
2023-12-05 13:17:15 +0700

    Lab9

--- Lab9/Program.cs
+++ Lab9/Program.cs
@@ -8,6 +8,9 @@ namespace Lab9
 {
     internal class Program
     {
+        static public string[] names = {"Торин", "Глоин", "Оин", "Балин", "Двалин", "Бофур", "Бифур", "Кили", "Фили",
+        "Дори", "Нори", "Ори", "Бомбур" };
+            
         static public List<Discipline> disciplines;
         static public List<Student> students;
 
@@ -19,6 +22,13 @@ namespace Lab9
                 new History(50, 5),
                 new Programming(10)
             };
+
+            students = new List<Student>();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+
+            }
         }
     }
 }
--- Lab9/Student.cs
+++ Lab9/Student.cs
@@ -13,6 +13,21 @@ namespace Lab9
         private Dictionary<IHavePractice, int> Practices { get; set; }
         private Dictionary<IHaveFinalControl, int> FinalControls { get; set; }
 
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="discipline"></param>
+        /// <param name="result"></param>
+        public void AddResult(IHaveFinalControl discipline, int result)
+        {
+
+        }
+
         /// <summary>
         /// Формирует отчет о автомате определенной дисциплины
         /// </summary>
@@ -20,7 +35,50 @@ namespace Lab9
         /// <returns>Отчет</returns>
         public string Check(Discipline discipline)
         {
+            bool pass = true;
+            string report = $"{discipline.Title}: ";
+            if(discipline is IHaveAngryTeacher)
+            {
+                report += "Автомат невозможен, учитель is злой";
+                return report;
+            }
 
+            if(discipline is IHavePractice practiceDiscipline && Practices.ContainsKey(practiceDiscipline))
+            {
+                if(practiceDiscipline.Check(Practices[practiceDiscipline]))
+                {
+                    report += "Практик достаточно, ";
+                }
+                else
+                {
+                    report += "Практик недостаточно, ";
+                    pass = false;
+                }
+            }
+
+            if (discipline is IHaveFinalControl controlDiscipline && FinalControls.ContainsKey(controlDiscipline))
+            {
+                if (controlDiscipline.Check(FinalControls[controlDiscipline]))
+                {
+                    report += "Баллов достаточно, ";
+                }
+                else
+                {
+                    report += "Баллов недостаточно, ";
+                    pass = false;
+                }
+            }
+
+            if (pass)
+            {
+                report += "Автомат будет";
+            }
+            else
+            {
+                report += "Автомата не будет";
+            }
+
+            return report;
         }
     }
 }
