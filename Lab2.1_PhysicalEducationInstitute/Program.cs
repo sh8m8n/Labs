db537437d47a6cc3572a6dff9e948fc4925100a2
sh8m8n <118104171+sh8m8n@users.noreply.github.com>
2024-02-21 23:21:42 +0700

    renaming

--- Lab2.1_PhysicalEducationInstitute/Program.cs
+++ Lab2.1_PhysicalEducationInstitute/Program.cs
@@ -1,9 +1,5 @@
 ﻿using StudentRegister;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhysicalEducationInstitute
 {
